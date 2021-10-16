4ea0f96de126a257b6d3281da4e731e59bda2f71
elbertribeiro <elbertprofissional@gmail.com>
2021-10-16 19:33:17 -0300

    Criação do Projeto

---
+++ src/main/java/comgithub/elbertribero/ifoodapi/IfoodapiApplication.java
@@ -0,0 +1,12 @@
+package comgithub.elbertribero.ifoodapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication public class IfoodapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IfoodapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/comgithub/elbertribero/ifoodapi/IfoodapiApplicationTests.java
@@ -0,0 +1,11 @@
+package comgithub.elbertribero.ifoodapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class IfoodapiApplicationTests {
+
+    @Test void contextLoads() {
+    }
+
+}
